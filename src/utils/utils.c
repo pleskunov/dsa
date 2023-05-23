eb5cce2b0381d3496b149731e4633d4122b04afa
Paul Pleskunov <papl@positroncloud.com>
2023-05-23 16:41:54 +0200

    user-defined functions skeleton added

---
+++ src/utils/utils.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "utils.h"
+
+int match(const void *key1, const void *key2) {
+  /* code */
+
+  return 0;
+}
+
+void destroy(void* data) {
+  /* code */
+
+  return;
+}
---
+++ src/utils/utils.h
@@ -0,0 +1,9 @@
+/* The library of user-defined functions. */
+
+#pragma once
+
+#include <stdlib.h>
+
+int match(const void *key1, const void *key2);
+
+void destroy(void* data);
