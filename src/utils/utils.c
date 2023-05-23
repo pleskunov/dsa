7331f008b22beedaac527e385854bc0fd56d5ac9
Paul Pleskunov <papl@positroncloud.com>
2023-05-23 17:21:01 +0200

    basic int and str match functions

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,10 +1,32 @@
 #include <stdlib.h>
+#include <string.h>
 #include "utils.h"
 
 int match(const void *key1, const void *key2) {
   /* code */
 
   return 0;
+}
+
+/* Default key comparisons. */
+
+int str_match(void *key1, void *key2) {
+  return strcmp((char *)key1, (char *)key2);
+}
+
+int int_match(void *key1, void *key2) {
+  int i1 = *((int *)key1);
+  int i2 = *((int *)key2);
+
+  if (i1 > i2) {
+    return 1;
+  }
+  else if (i1 == i2) {
+    return 0;
+  }
+  else {
+    return -1;
+  }
 }
 
 void destroy(void* data) {
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -4,6 +4,16 @@
 
 #include <stdlib.h>
 
+/* Default key comparisons. Format: <type> <name>match(void *key1, void *key2); */
+
+/* Generic type example */
 int match(const void *key1, const void *key2);
 
+/* String keys. */
+int str_match(void *key1, void *key2);
+
+/* Integer keys. */
+int int_match(void *key1, void *key2);
+
+/* TODO */
 void destroy(void* data);
