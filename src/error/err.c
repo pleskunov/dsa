e822de5edc0e15338c2f69070b9634f50dc3a28a
Paul Pleskunov <pp@positroncloud.com>
2025-03-24 21:56:02 -0400

    Cleanup, bugfixes, correct handling callbacks

--- src/error/err.c
+++ src/error/err.c
@@ -1,33 +1,58 @@
-#include "err.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Global variable to hold the callback function */
+#include "err.h"
+
+/* Global variables to hold the callback functions */
 static error_callback_t error_callback = NULL;
+static error_callback_t unrecoverable_error_callback = NULL;
 
-_Noreturn static void fallback_unrecoverable_error_handler(const char *message) {
-  fprintf(stdout, "Unrecoverable Error: %s\n", message);
-  exit(EXIT_FAILURE);
+/* Error handling module functions */
+static void fallback_error_handler(const char *message, callback_type_t type) {
+  if (type == UNRECOVERABLE) {
+    fprintf(stdout, "Unrecoverable Error: %s\n", message);
+
+    exit(EXIT_FAILURE);
+  } else {
+    fprintf(stdout, "Error: %s\n", message);
+
+    return;
+  }
 }
 
-static void fallback_error_handler(const char *message) {
-  fprintf(stdout, "Error: %s\n", message);
+void set_error_callback(error_callback_t callback, callback_type_t type) {
+  switch (type) {
+    case RECOVERABLE:
+      error_callback = callback;
+      break;
+    case UNRECOVERABLE:
+      unrecoverable_error_callback = callback;
+      break;
+    default:
+      /* NO-OP */
+      break;
+  }
 }
 
-void set_error_callback(error_callback_t callback) {
-  error_callback = callback;
-}
-
-void reset_error_callback(void) {
-  error_callback = NULL;
+void reset_error_callback(callback_type_t type) {
+  switch (type) {
+    case RECOVERABLE:
+      error_callback = NULL;
+      break;
+    case UNRECOVERABLE:
+      unrecoverable_error_callback = NULL;
+      break;
+    default:
+      /* NO-OP */
+      break;
+  }
 }
 
 void report_unrecoverable_error(const char *message) {
-  if (error_callback) {
-    error_callback(message); // Call the user-defined callback (must be div!)
+  if (unrecoverable_error_callback) {
+    unrecoverable_error_callback(message); // Call the user-defined callback (must be div!)
   } else {
-    fallback_unrecoverable_error_handler(message);
+    fallback_error_handler(message, UNRECOVERABLE);
   }
 
   /* This function should never return */
@@ -38,6 +63,6 @@ void report_error(const char *message) {
   if (error_callback) {
     error_callback(message);
   } else {
-    fallback_error_handler(message);
+    fallback_error_handler(message, RECOVERABLE);
   }
 }
--- src/error/err.h
+++ src/error/err.h
@@ -3,11 +3,13 @@
 /* An error callback function type */
 typedef void (*error_callback_t)(const char *message);
 
-void set_error_callback(error_callback_t callback);
+typedef enum {
+  RECOVERABLE,
+  UNRECOVERABLE
+} callback_type_t; 
 
-void reset_error_callback(void);
-
+void set_error_callback(error_callback_t callback, callback_type_t type);
+void reset_error_callback(callback_type_t type);
 // _Noreturn void report_error(const char *message); // C11
 void report_unrecoverable_error(const char *message) __attribute__((noreturn));
-
 void report_error(const char *message);
