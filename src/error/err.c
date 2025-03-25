9627e6d04af095995d4a1265b4e9fe44064d8f9a
Paul Pleskunov <pp@positroncloud.com>
2025-03-24 21:14:32 -0400

    Split errors into recoverable and not

--- src/error/err.c
+++ src/error/err.c
@@ -6,9 +6,13 @@
 /* Global variable to hold the callback function */
 static error_callback_t error_callback = NULL;
 
-_Noreturn static void fallback_error_handler(const char *message) {
-  fprintf(stdout, "Error: %s\n", message);
+_Noreturn static void fallback_unrecoverable_error_handler(const char *message) {
+  fprintf(stdout, "Unrecoverable Error: %s\n", message);
   exit(EXIT_FAILURE);
+}
+
+static void fallback_error_handler(const char *message) {
+  fprintf(stdout, "Error: %s\n", message);
 }
 
 void set_error_callback(error_callback_t callback) {
@@ -18,13 +22,22 @@ void set_error_callback(error_callback_t callback) {
 void reset_error_callback(void) {
   error_callback = NULL;
 }
-void report_error(const char *message) {
+
+void report_unrecoverable_error(const char *message) {
   if (error_callback) {
-    error_callback(message); // Call the user-defined callback
+    error_callback(message); // Call the user-defined callback (must be div!)
   } else {
-    fallback_error_handler(message);
+    fallback_unrecoverable_error_handler(message);
   }
 
   /* This function should never return */
   exit(EXIT_FAILURE);
+}
+
+void report_error(const char *message) {
+  if (error_callback) {
+    error_callback(message);
+  } else {
+    fallback_error_handler(message);
+  }
 }
--- src/error/err.h
+++ src/error/err.h
@@ -8,4 +8,6 @@ void set_error_callback(error_callback_t callback);
 void reset_error_callback(void);
 
 // _Noreturn void report_error(const char *message); // C11
-void report_error(const char *message) __attribute__((noreturn));
+void report_unrecoverable_error(const char *message) __attribute__((noreturn));
+
+void report_error(const char *message);
